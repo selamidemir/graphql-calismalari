83c5dfec7b3feb8ffd93637cde2f5e06ee31489c
Selami Demir <selamidemir@gmail.com>
2023-01-07 12:25:11 +0300

    Subscribe işlemelerine devam.

---
+++ app3.js
@@ -0,0 +1,18 @@
+const { createYoga, createSchema, createPubSub } = require("graphql-yoga");
+const { createServer } = require("node:http");
+
+const {typeDefs} = require("./assets/typeDefs");
+const {resolvers} = require("./assets/resolvers3")
+
+// Provide your schema
+const yoga = createYoga({
+  schema: createSchema({
+    typeDefs,
+    resolvers
+  }),
+});
+
+const server = createServer(yoga);
+server.listen(4000, () => {
+  console.info("Server is running on http://localhost:4000/graphql");
+});
